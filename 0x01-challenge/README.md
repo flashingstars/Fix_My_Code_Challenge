8f63c42f321cba19a688749cf6bc30e71b780ebd
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-17 06:07:11 -0700

    Square fix

---
+++ 0x01-challenge/README.md
@@ -0,0 +1,1 @@
+This folder contains tasks from other github repositories that have been fixed for accuracy.
---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" A module defining a Square module"""
+
+
+class Square():
+    """ Class that defines a square """
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the Square """
+        return self.width * self.height
+
+    def PermiterOfMySquare(self):
+        """ Perimeter of the Square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ A Square object representation """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
